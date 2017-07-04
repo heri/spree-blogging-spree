877f95dc5655b69223eb42cf0f5f578941247fe8
Heri <heri@studiozenkai.com>
2017-07-04 15:55:10 -0400

    - Larger blog image when showing blog entries

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment, 
-     :styles => {:mini => '48x48#', :normal => '240x240>', :large => '600x600>'},
+     :styles => {:mini => '48x48#', :normal => '350x300>', :large => '847x564>'},
      :default_style => :large,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
      :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"  
