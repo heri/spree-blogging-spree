6b235c66d183c6ba1aad3880e84631ece9be0b6b
Heri <heri@studiozenkai.com>
2017-08-02 15:06:34 -0400

    Forced resizing of images

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment, 
-     :styles => {:mini => '48x48#', :normal => '348x303>', :large => '847x564>'},
+     :styles => {:mini => '48x48#', :normal => '348x302#', :large => '847x564>'},
      :default_style => :large,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
      :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"  
